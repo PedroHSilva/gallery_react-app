0f97ecdc2c0c3a30a74b9b9cad43d45d5fa88614
Pedro Silva <pedrohsilva@Pedro-pc.localdomain>
2023-01-10 19:11:24 -0300

    Primeiros componetes: Cabecalho, Banner e Menu

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,9 +1,21 @@
 import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+import banner from './banner.png'
+import styles from './PaginaInicial.module.scss'
 
 export default function Paginainicial () {
     return (
-        <>
-            <Cabecalho/>
-        </>
+    <>
+        <Cabecalho/>
+        <main>
+            <section className={styles.principal}>
+                <Menu/>
+                <div className={styles.principal__imagem}>
+                    <h1>Gallery React App</h1>
+                    <img src={banner} alt="" />
+                </div>
+            </section>
+        </main>
+    </>
     )
 }
